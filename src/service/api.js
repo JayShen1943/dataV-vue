6fffb4caba0cc477aa4261bb0cdda33a5b1b27ef
JayShen <8069927@qq.com>
2021-03-15 21:16:06 +0800

    配置生产

--- src/service/api.js
+++ src/service/api.js
@@ -3,19 +3,12 @@
  * @Author: JayShen
  * @Date: 2021-02-27 21:50:09
  * @LastEditors: JayShen
- * @LastEditTime: 2021-03-06 09:36:56
+ * @LastEditTime: 2021-03-15 21:14:58
  */
 import request from '@/utils/request'
 // import {
 //     getToken
 // } from '@/utils/token'
-export function demo(params) {
-    return request({
-        url: '/proxy/demo',
-        method: 'get',
-        params
-    })
-}
 // 中间大屏中间数据
 export function findCenterScreenDataMiddle(params) {
     return request({
