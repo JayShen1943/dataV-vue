38a5be00d82f451f61918de2f2c8e1e0d47b6ba2
JayShen <jie.shen@deti.cn>
2021-03-31 23:55:22 +0800

    feat:主页注释监控

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -3,7 +3,7 @@
  * @Author: JayShen
  * @Date: 2021-03-02 15:06:12
  * @LastEditors: JayShen
- * @LastEditTime: 2021-03-30 13:43:33
+ * @LastEditTime: 2021-03-31 23:54:41
 -->
 <template>
   <div>
@@ -11,7 +11,7 @@
     <div class="jump-title" @click="jump('/leftScreen')">左侧副屏</div>
     <div class="jump-title" @click="jump('/rightScreen')">右侧副屏</div>
     <div>
-      <div class="liveView">
+      <!-- <div class="liveView">
         <video-player
           class="vjs-custom-skin"
           ref="videoPlayer"
@@ -21,21 +21,21 @@
           :playsinline="playsinline"
         >
         </video-player>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
-import { videoPlayer } from "vue-video-player";
+// import { videoPlayer } from "vue-video-player";
 import "video.js/dist/video-js.css";
 import "vue-video-player/src/custom-theme.css";
 import "videojs-contrib-hls";
 export default {
   name: "Home",
-  components: {
-    videoPlayer,
-  },
+  // components: {
+  //   videoPlayer,
+  // },
   data() {
     return {
       initialized: false,
@@ -68,40 +68,40 @@ export default {
       },
     };
   },
-  computed: {
-    player() {
-      return this.$refs.videoPlayer.player;
-    },
-    currentStream() {
-      return this.currentTech === "Flash" ? "RTMP" : "HLS";
-    },
-    playsinline() {
-      let ua = navigator.userAgent.toLocaleLowerCase();
-      // x5内核
-      if (ua.match(/tencenttraveler/) != null || ua.match(/qqbrowse/) != null) {
-        return false;
-      } else {
-        // ios端
-        return true;
-      }
-    },
-  },
+  // computed: {
+  //   player() {
+  //     return this.$refs.videoPlayer.player;
+  //   },
+  //   currentStream() {
+  //     return this.currentTech === "Flash" ? "RTMP" : "HLS";
+  //   },
+  //   playsinline() {
+  //     let ua = navigator.userAgent.toLocaleLowerCase();
+  //     // x5内核
+  //     if (ua.match(/tencenttraveler/) != null || ua.match(/qqbrowse/) != null) {
+  //       return false;
+  //     } else {
+  //       // ios端
+  //       return true;
+  //     }
+  //   },
+  // },
   methods: {
     jump(url) {
       this.$router.push({
         path: url,
       });
     },
-    onPlayerReadied() {
-      if (!this.initialized) {
-        this.initialized = true;
-        this.currentTech = this.player.techName_;
-      }
-    },
-    // record current time
-    onTimeupdate(e) {
-      console.log("currentTime", e.cache_.currentTime);
-    },
+    // onPlayerReadied() {
+    //   if (!this.initialized) {
+    //     this.initialized = true;
+    //     this.currentTech = this.player.techName_;
+    //   }
+    // },
+    // // record current time
+    // onTimeupdate(e) {
+    //   console.log("currentTime", e.cache_.currentTime);
+    // },
   },
 };
 </script>
